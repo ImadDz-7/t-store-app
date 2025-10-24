a6d366217efb75a0c99f8b039a858f6cfd31989d
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-10-24 11:49:14 +0000

    Create TElevatedButtonTheme class for light & dark theme

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TElevatedButtonTheme{
+  TElevatedButtonTheme._(); // To avoid creating instances
+
+  /// -- Light Theme
+  static final lightElevatedButton = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      
+    ),
+  );
+
+
+}
