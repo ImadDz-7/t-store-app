302c56bfe145df5005cb6abdb2f40d0190b8b1c6
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-11-21 14:45:12 +0000

    Adding Footer ( Google & Facebook ) IconButtons

--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -138,6 +138,42 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ],
               ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Footer
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: TColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: (){},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.google),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: TColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: (){},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.facebook),
+                      ),
+                    ),
+                  ),
+              ],),
             ],
           ),
         ),
